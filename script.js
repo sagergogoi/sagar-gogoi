8342b18726988c62beefd0a703ac0505afd1e0f0
Sagar . <fanindragogoi@gmail.com>
2024-11-07 23:15:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const navmain = document.getElementById('navmain');    //getelementbyId(name of id) returns id to the variable 
+
+function handleMenu(){                                  //element.classlist return the class if the element or the id
+    navmain.classList.toggle('hidden')                  //toggle 
+
+}
+
+
+function setupIntersectionObserver(element,isLTR,speed){
+    
+
+}
